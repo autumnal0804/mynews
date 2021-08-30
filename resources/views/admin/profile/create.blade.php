ac8422277160a0a5efe49d6ea867dc50e4285dc1
kou.yuiyui@gmail.com <ec2-user@ip-172-31-20-177.us-east-2.compute.internal>
2021-08-30 07:32:50 +0000

    mynews

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset ="UTF-8">
-        <meta http-equiv="X-UA-Compatible"content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MyNews(create)</title>
-    </head>
-    <body>
-        <h1>Myニュース作成画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+        
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
